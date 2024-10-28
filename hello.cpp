4466ede9e26e079ccdc2b0260610df55f747af49
Charlie Hartley <JCHartley@scu.edu>
2024-10-28 15:13:46 -0700

    ChangesToHelloInMyBranch

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main () {
   cout << "Hello World" << endl;
-  cout << "Changes to hello from BRANCH" << endl;
+  cout << "I am now editing hello in myBranch. After the merge." << endl;
   MyClass a(5);
   cout << a.getX() << endl;
 }
