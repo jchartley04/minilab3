96091b4a94a99aa98abbf21afb24f7e048937ff0
Charlie Hartley <JCHartley@scu.edu>
2024-10-28 15:09:29 -0700

    CommitAfterMerge

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main () {
   cout << "Hello World" << endl;
-  cout << "Changes to hello from BRANCH" << endl;
+  cout << "Changes to hello after merge" << endl;
   MyClass a(5);
   cout << a.getX() << endl;
 }
