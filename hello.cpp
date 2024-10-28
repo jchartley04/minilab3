bb23f03aef6961406e15b3fecd4622d0c6655564
Charlie Hartley <JCHartley@scu.edu>
2024-10-28 15:06:34 -0700

    Branch Commit

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main () {
   cout << "Hello World" << endl;
-  cout << "Changes to hello" << endl;
+  cout << "Changes to hello from BRANCH" << endl;
   MyClass a(5);
   cout << a.getX() << endl;
 }
